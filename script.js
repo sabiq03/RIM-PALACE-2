983269d7ea32368283c73d7bf07fd1b2749256cc
sabiq03 <117346761+sabiq03@users.noreply.github.com>
2026-06-11 15:00:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// const hero = document.querySelector(".hero");
+
+
+// const images = [
+//   '/images_mahal/mahalimg1.jpeg',
+//   '/images_mahal/mahalimg2.jpeg',
+//   'images_mahal/mahalimg7.png'
+//   ];
+// let i = 0;
+
+// // Add smooth transition using CSS
+
+// setInterval(() => {
+// i++;
+// if (i >= images.length) i = 0;
+// hero.style.backgroundImage = `url(${images[i]})`;
+// },3000);
+
+// const slides = document.querySelectorAll(".slide");
+
+// const images = [
+//   "images_mahal/mahalimg5.png",
+//   "images_mahal/mahalimg1.jpeg",
+//   "images_mahal/mahalimg2.jpeg",
+//   "images_mahal/mahalimg7.png"
+// ];
+
+// let i = 0;
+
+// // Set first image
+// slides[0].style.backgroundImage = `url(${images[0]})`;
+// slides[1].style.backgroundImage = `url(${images[1]})`;
+
+// setInterval(() => {
+//   slides[0].classList.toggle("active");
+//   slides[1].classList.toggle("active");
+
+//   i = (i + 1) % images.length;
+
+//   setTimeout(() => {
+//     slides[0].style.backgroundImage = `url(${images[i]})`;
+//   }, 1000);
+
+// }, 4000);
